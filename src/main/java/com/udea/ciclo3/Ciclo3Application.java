884e17f45145bf6114cd9ece13b0c5e0f6c2bd3d
Giancarlo <gfzc621@gmail.com>
2022-08-31 22:28:40 -0500

    Saludo web y modificacion en porperties

--- src/main/java/com/udea/ciclo3/Ciclo3Application.java
+++ src/main/java/com/udea/ciclo3/Ciclo3Application.java
@@ -1,13 +1,27 @@
 package com.udea.ciclo3;
 
+import net.minidev.json.JSONUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+import javax.xml.bind.SchemaOutputResolver;
+
+@RestController
+@SpringBootApplication (exclude ={SecurityAutoConfiguration.class})
+
 public class Ciclo3Application {
+    @GetMapping("/hola")
+    public String hola() {
+        return "Hola este es el primer saludo web";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ciclo3Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Ciclo3Application.class, args);
+        System.out.println("Hola mundo");
+    }
+
 
 }
