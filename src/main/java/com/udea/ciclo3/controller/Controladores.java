5c2c9f05b3bff7bfcaf6f5f6034b3947d6c57d64
Giancarlo <gfzc621@gmail.com>
2022-09-08 21:20:25 -0500

    Ultima actualizacion

--- src/main/java/com/udea/ciclo3/controller/Controladores.java
+++ src/main/java/com/udea/ciclo3/controller/Controladores.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -18,38 +18,64 @@ public class Controladores {
     @Autowired //Inyeccion de metodos con services
     EmpresaService empresaService; //Creo objeto de tipo EmpresaService para usar metodos del servicio creado
 
-    @GetMapping({"/","/VerEmpresas"}) //Anotacion que mapea y crea el servicio VerEmpresas
-    public String viewEmpresas(Model model){ //Metodo para obtener cadenas de empresas con objeto de clase tipo Model, recibe cualquiercosa y modela segun lo necesitado
-        List <Empresa> listEmpresas = empresaService.getAllEmpresas(); //Se crea lista de tipo Empresas con el metodo getAllEmpresas
+    //Metodo para ver Empresas
+    @GetMapping({"/", "/VerEmpresas"}) //Anotacion que mapea y crea el servicio VerEmpresas
+    public String verEmpresas(Model model) { //Metodo para obtener cadenas de empresas con objeto de clase tipo Model, recibe cualquiercosa y modela segun lo necesitado
+        List<Empresa> listEmpresas = empresaService.getAllEmpresas(); //Se crea lista de tipo Empresas con el metodo getAllEmpresas
         model.addAttribute("emplist", listEmpresas); //Sa alimenta objeto model con lista de empresas
         return "verEmpresas"; //LLama a pagina html creada
     }
 
+    //Metodo par para agregar Empresas
     @GetMapping({"/AgregarEmpresa"}) //Anotacion que mapea y crea el servicio AgregarEmpresa obtener informacion
-    public String newEmpresa(Model model){ //Inserta objeto model
+    public String nuevaEmpresa(Model model) { //Inserta objeto model
         Empresa nuevaEmp = new Empresa(); //Crea objeto de tipo Empresa para crear la nueva empresa utilizando el constructor vacio
-        model.addAttribute("newEmpn", nuevaEmp); //Envia el objeto por medio de model
+        model.addAttribute("nuevaEmp", nuevaEmp); //Envia el objeto por medio de model
         return "agregarEmpresas"; //LLama a pagina html creada
 
     }
 
+    //Metodo par guardar Empresas
     @PostMapping("/GuardarEmpresa")
-    public String guardarEmpresa(Empresa nuevaEmp, RedirectAttributes redirectAttributes){
-        if (empresaService.saveOrUpdateEmpresa(nuevaEmp)==true){
+    public String guardarEmpresa(Empresa nuevaEmp, RedirectAttributes redirectAttributes) {
+        if (empresaService.saveOrUpdateEmpresa(nuevaEmp) == true) {
             return "redirect:/VerEmpresas";
         }
         return "redirect:/AgregarEmpresa";
     }
 
-    @GetMapping("/EditarEmpresa")
-    public String editEmpresa(Model model, @PathVariable Long id){
+    //Metodo par editar Empresas
+    @GetMapping("/EditarEmpresa/{id}")
+    public String editarEmpresa(Model model, @PathVariable Long id) { //Elimina id que llega en la ruta con @PathVariable
         Empresa editEmp = empresaService.getEmpresaById(id);
+        //Crea atributo para el modelo llamado igual que el objeto creado que va al html y llena campos de ese objeto
         model.addAttribute("editEmp", editEmp);
         return "editarEmpresa";
+    }
 
+    //Metodo para actualizar Empresas
+    @PostMapping("/ActualizarEmpresa")
+    public String ActualizarEmpresa(@ModelAttribute("actEmpresa") Empresa actEmpresa) {
+        if (empresaService.saveOrUpdateEmpresa(actEmpresa)) {
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/EditarEmpresa";
 
     }
 
+    @GetMapping("/EliminarEmpresa/{id}")
+    public String eliminarEmpresa(@PathVariable Long id) {
+       try {
+           empresaService.deleteEmpresa(id);
+           return "redirect:/VerEmpresas";
+       }catch(Exception e){
+            return "redirect:/VerEmpresas";
+
+        }
+
+
+
+    }
 
 
 }
